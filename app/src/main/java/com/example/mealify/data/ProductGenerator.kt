2a220e87188f93c39dea7a163e7d7e22b2b33f4e
Gijs Kruize <gbmkruize@gmail.com>
2022-10-18 17:48:37 +0200

    werkt niet

--- app/src/main/java/com/example/mealify/data/ProductGenerator.kt
+++ app/src/main/java/com/example/mealify/data/ProductGenerator.kt
@@ -1,19 +1,16 @@
 package com.example.mealify.data
 
-import org.jsoup.examples
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+
+
 
 
 object ProductGenerator {
     private fun webPageReader(input: String): String {
-        val url = URL(input)
-        val urlConnection = url.openConnection() as HttpsURLConnection
 
-        Jsoup.parse(htmlString).title()
-        try {
-            return urlConnection.inputStream.bufferedReader().readText()
-        } finally {
-            urlConnection.disconnect()
-        }
+        val doc : Document = Jsoup.connect(input).get()
+        return doc.select("p").text()
     }
 
      fun generateProductsArray(input: String): Array<String> {
